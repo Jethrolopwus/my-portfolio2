e9497f056576d586171a0932570f86643bea7d90
Jethrolopwus <lopwusjethro92@gmail.com>
2024-02-20 05:01:43 -0800

    chore: update the NavLink components

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,12 +1,12 @@
-
-
 import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
   return (
     <li>
-      <Link href={href} className="block py-2 pl-3 pr-4 text-slate-300 sm:text-xl rounded md:p-0 hover:text-white" >
+      <Link href={href}>
+        <a className="block py-2 pl-3 pr-4 text-slate-300 sm:text-xl rounded md:p-0 hover:text-white">
           {title}
+        </a>
       </Link>
     </li>
   );
