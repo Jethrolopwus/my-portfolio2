778aaa9ad341c025612a5dc9da35a8b3169d7106
Jethrolopwus <lopwusjethro92@gmail.com>
2024-05-09 09:43:54 +0000

    chore: update the application

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
-import AboutSection from "./components/AboutSection";
-import ProjectSection from "./components/ProjectSection";
-import EmailSection from "./components/EmailSection";
-import Footer from "./components/Footer";
-import Achievements from "./components/Achievements";
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/Navbar";
+import AboutSection from "../components/AboutSection";
+import ProjectSection from "../components/ProjectSection";
+import EmailSection from "../components/EmailSection";
+import Footer from "../components/Footer";
+import Achievements from "../components/Achievements";
 
 
 export default function Home() {
